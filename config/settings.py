a9f430a36c7bb1fa403995041b6b5a2ef501500f
Tran Nguyen <nguyentran28112005@gmail.com>
2026-05-22 21:30:20 +0700

    Fix Gmail SMTPSenderRefused 500 error

--- config/settings.py
+++ config/settings.py
@@ -164,7 +164,7 @@ EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 EMAIL_USE_SSL = config('EMAIL_USE_SSL', default=False, cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='Urban Manager <noreply@urbanmanager.com>')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default=EMAIL_HOST_USER)
 
 
 # ==============================================================
